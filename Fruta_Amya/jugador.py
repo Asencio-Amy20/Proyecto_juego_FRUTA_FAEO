09bb0c491942c737ab93dde4f64e7b92442bb22d
LysBorbor13 <borborlys@gmail.com>
2025-11-25 07:52:00 -0500

    Arreglo1

--- Fruta_Amya/jugador.py
+++ Fruta_Amya/jugador.py
@@ -13,21 +13,21 @@ class Jugador:
             self.imagen.fill((0, 0, 255))
 
     def mover(self, teclas, velocidad=5):
-    
-    if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
-        self.x -= velocidad
-    if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
-        self.x += velocidad
-    if teclas[pygame.K_UP] or teclas[pygame.K_w]:
-        self.y -= velocidad
-    if teclas[pygame.K_DOWN] or teclas[pygame.K_s]:
-        self.y += velocidad
-    
-    
-    self.x = max(0, min(self.x, 740))
-    self.y = max(0, min(self.y, 540))
-    
+        if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
+            self.x -= velocidad
+
+        if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
+            self.x += velocidad
+
+        if teclas[pygame.K_UP] or teclas[pygame.K_w]:
+            self.y -= velocidad
+
+        if teclas[pygame.K_DOWN] or teclas[pygame.K_s]:
+            self.y += velocidad
+
+        
+        self.x = max(0, min(self.x, 740))
+        self.y = max(0, min(self.y, 540))
+
     def dibujar(self, pantalla):
         pantalla.blit(self.imagen, (self.x, self.y))
-
-
