596de79e8b1e7d2d0b8ea352314ce9567627bbe7
LysBorbor13 <borborlys@gmail.com>
2025-11-25 09:02:29 -0500

    Add obtener_pos method to Jugador class
    Added a method to get the player's position.

--- Fruta_Amya/jugador.py
+++ Fruta_Amya/jugador.py
@@ -3,8 +3,9 @@ import pygame
 class Jugador:
     def __init__(self, x, y):
         self.x = x
-        self.y = y
+       	self.y = y
         self.velocidad = 5
+
         try:
             imagen_original = pygame.image.load("assets/jugador/jugador.png")
             self.imagen = pygame.transform.scale(imagen_original, (60, 60))
@@ -15,19 +16,19 @@ class Jugador:
     def mover(self, teclas, velocidad=5):
         if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
             self.x -= velocidad
-
         if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
             self.x += velocidad
-
         if teclas[pygame.K_UP] or teclas[pygame.K_w]:
             self.y -= velocidad
-
         if teclas[pygame.K_DOWN] or teclas[pygame.K_s]:
             self.y += velocidad
 
-        
         self.x = max(0, min(self.x, 740))
         self.y = max(0, min(self.y, 540))
 
     def dibujar(self, pantalla):
         pantalla.blit(self.imagen, (self.x, self.y))
+
+ 
+    def obtener_pos(self):
+        return self.x, self.y
